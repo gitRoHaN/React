c47a841d3dfc7bba8e4471ac247598882cabda31
Rohan <rohankadam2703@gmail.com>
2023-10-10 07:29:26 +0530

    Final commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,58 @@
-import { render, screen } from '@testing-library/react';
-import HomePage from './Homepage';
+import { render, screen, act, userEvent } from '@testing-library/react';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import Main from './Main';
+import { fireEvent } from '@testing-library/react';
+import BookingForm from './BookingForm';
+import HomePage from './Homepage';
+
 test('renders the booking form heading', () => {
   render(<HomePage />);
   const linkElement = screen.getByText("Special Offer");
   expect(linkElement).toBeInTheDocument();
 });
-test('testing function',()=>{
-  render(<Main/>);
-}); 
 
+
+
+test('renders the header heading', () => {
+  render(<BrowserRouter><Main /></BrowserRouter>);
+  const headingElement = screen.getByText('Little Lemon');
+  expect(headingElement).toBeInTheDocument();
+
+  const reserveButton = screen.getByRole("button");
+  fireEvent.click(reserveButton);
+});
+
+test('Initialize/Update Times', () => {
+  render(<BrowserRouter><App /></BrowserRouter>);
+  const reserveButton = screen.getByRole("button");
+  fireEvent.click(reserveButton);
+});
+
+describe("Booking Form", () => {
+
+  test('Shows available times after selecting a date', () => {
+    render(<BrowserRouter><BookingForm availableTimes={["17:00", "18:00", "19:00"]} /></BrowserRouter>);
+    const dateInput = screen.getByLabelText("Choose date");
+
+    act(() => {
+      fireEvent.change(dateInput, { target: { value: "2023-06-05" } });
+    });
+
+    const timeSelect = screen.getByLabelText("Choose time");
+    expect(timeSelect).toBeInTheDocument();
+    const timeOptions = screen.getAllByRole("option");
+    expect(timeOptions).toHaveLength(7);
+  });
+})
+
+test("checks validation", () => {
+  const handleSubmit = jest.fn()
+  render(<BrowserRouter><BookingForm onSubmit={handleSubmit} /></BrowserRouter>);
+
+  const reserveButton = screen.getByRole('button')
+  fireEvent.click(reserveButton);
+
+  expect(handleSubmit).not.toHaveBeenCalled();
+})
 
